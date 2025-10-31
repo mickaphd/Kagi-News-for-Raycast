6adccb64244005ab4e29873155e8d6c307b75f7b
mickaphd <mickaphd@users.noreply.github.com>
2025-10-31 14:30:40 +0100

    Add files via upload

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -9,7 +9,27 @@
 
 type ExtensionPreferences = {
   /** Language - Article language */
-  "language": "default" | "en" | "es" | "fr" | "de" | "it" | "pt" | "pt-BR" | "ja" | "ko" | "zh-Hans" | "zh-Hant" | "nl" | "pl" | "ru" | "sv" | "fi" | "nb" | "tr" | "th" | "uk" | "ca" | "hi" | "ar" | "he" | "lb"
+  "language": "default" | "en" | "es" | "fr" | "de" | "it" | "pt" | "pt-BR" | "ja" | "ko" | "zh-Hans" | "zh-Hant" | "nl" | "pl" | "ru" | "sv" | "fi" | "nb" | "tr" | "th" | "uk" | "ca" | "hi" | "ar" | "he" | "lb",
+  /** Display Sections - Display the main article image */
+  "showPrimaryImage": boolean,
+  /**  - Display key talking points from the article */
+  "showTalkingPoints": boolean,
+  /**  - Display the secondary article image */
+  "showSecondaryImage": boolean,
+  /**  - Display different perspectives on the story */
+  "showPerspectives": boolean,
+  /**  - Display historical context */
+  "showHistoricalBackground": boolean,
+  /**  - Display technical information */
+  "showTechnicalDetails": boolean,
+  /**  - Display industry-related impacts */
+  "showIndustryImpact": boolean,
+  /**  - Display timeline of events */
+  "showTimeline": boolean,
+  /**  - Display reactions from international entities */
+  "showInternationalReactions": boolean,
+  /**  - Display the 'Did You Know' section */
+  "showDidYouKnow": boolean
 }
 
 /** Preferences accessible in all the extension's commands */
